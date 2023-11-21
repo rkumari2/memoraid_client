ed42b29031cb51c346dbd6593c2a60b80c6fa603
“rkumari2” <rubinakumari@mail.com>
2023-11-21 19:51:47 +0000

    right answers counter and percentage functioning

--- src/pages/Flashcards/index.jsx
+++ src/pages/Flashcards/index.jsx
@@ -7,9 +7,18 @@ const Flashcards = () => {
     const { selectedSubjectId } = useSubject()
 
     const [results, setResults] = useState([])
-    const [ revealAnswer, setRevealAnswer ] = useState(false)
+    const [revealAnswer, setRevealAnswer] = useState(false)
+    const [totalQuestions, setTotalQuestions] = useState(0)
+    const [rightAnswers, setRightAnswers] = useState(0)
+    const [wrongAnswers, setWrongAnswers] = useState(0)
+    const [isEndClicked, setIsEndClicked] = useState(false)
+    const [finalPercentage, setFinalPercentage] = useState(0)
 
     // console.log('flashcard page id of subject:', selectedSubjectId)
+
+    console.log('right answers', rightAnswers)
+    console.log('total questions', totalQuestions)
+    console.log(isEndClicked)
 
     useEffect(() => {
         const storedSubjectId = parseInt(localStorage.getItem('selectedSubjectId'), 10)
@@ -55,16 +64,50 @@ const Flashcards = () => {
         fetchData(selectedSubjectId);
     };
 
+    const handleRightAnswer = () => {
+        setTotalQuestions(totalQuestions + 1)
+        setRightAnswers(rightAnswers + 1)
+        fetchData(selectedSubjectId)
+    }
+
+    const handleWrongAnswer = () => {
+        setTotalQuestions(totalQuestions + 1)
+        setWrongAnswers(wrongAnswers + 1)
+        fetchData(selectedSubjectId)
+    }
+
+    const handleEndButton = () => {
+        const percentage = (rightAnswers / totalQuestions)*100
+        setFinalPercentage(percentage)
+        setIsEndClicked(true)
+    }
+
+    const handleRestart = () => {
+        setRevealAnswer(false)
+        setTotalQuestions(0)
+        setRightAnswers(0)
+        setWrongAnswers(0)
+        setFinalPercentage(0)
+        setIsEndClicked(false)
+        fetchData(selectedSubjectId)
+    }
+
     return (
         <div className='page-layout'>
             <h1>This is the Flashcards Page</h1>
 
-            <ul>
-                { results.length === 0 && (<h2> No Flashcards </h2>)}
+            {isEndClicked ? (
+                <div>
+                <h1> your final score is {finalPercentage}%, you did {rightAnswers} questions correct out of {totalQuestions} </h1>
+                <button onClick={handleRestart}> Start again </button> 
+            </div>
+            ) : (
+                <div> 
+                    <ul>
+                    { results.length === 0 && (<h2> No Flashcards </h2>)}
+                    </ul>
 
-            </ul>
-
-            {revealAnswer ? (
+                    {revealAnswer ? (
                 <div onClick={handleHideAnswer}> {results.map((item) => (
                     <h3> {item.answer} </h3>
                 ))} </div>
@@ -74,7 +117,14 @@ const Flashcards = () => {
                 ))} </div>
             )}
 
-            <button onClick={handleNextQuestion}>Next Question</button>
+<button onClick={handleRightAnswer}>right</button>
+            <button onClick={handleWrongAnswer}>wrong</button>
+            <button onClick={handleEndButton}>end</button>
+
+                </div>
+            )}
+
+            
 
         </div>
     )
