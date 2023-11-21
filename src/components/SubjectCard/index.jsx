f3b21cb5f43894e7fb14775f60c4d837a76c177c
“rkumari2” <rubinakumari@mail.com>
2023-11-21 12:16:29 +0000

    adding new subject functioning

--- src/components/SubjectCard/index.jsx
+++ src/components/SubjectCard/index.jsx
@@ -8,8 +8,43 @@ const SubjectCard = () => {
     const { responseToken } = useAuth()
 
     const [ results, setResults ] = useState([])
+    const [ showAddOverlay, setShowAddOverlay ] = useState(false)
+    const [ subject, setSubject ] = useState('')
 
     const navigate = useNavigate()
+
+    const handleShowOverlay = () => {
+        setShowAddOverlay(true)
+    }
+
+    const handleHideOverlay = () => {
+        setShowAddOverlay(false)
+        setSubject('')
+    }
+
+    const handleAddNew = async () => {
+        try {
+            const response = await axios.post(`https://memoraide-server.onrender.com/subjects/${responseToken.user_id}/new`, {
+                subject: subject
+            })
+            if (response.status === 201) {
+                const newSubject = {
+                    subject: subject, 
+                    id: response.data.id,
+                    user_id: responseToken.user_id
+                }
+
+                if (newSubject && Object.keys(newSubject).length > 0) {
+                    setResults((prevResults) => [...prevResults, newSubject])
+                    handleHideOverlay()
+                } else {
+                    console.error('Invalid new task data in the API response.')
+                }
+            }
+        } catch (err) {
+            console.error('Error adding new subject:', err)
+        }
+    }
 
     useEffect(() => {
 
@@ -49,6 +84,28 @@ const SubjectCard = () => {
                     </li>
             ))}
         </ul>
+
+        <button onClick={handleShowOverlay}> + </button>
+
+        <div id='add-overlay' style={{ display: showAddOverlay ? 'block' : 'none'}}>
+
+            {showAddOverlay && (
+                <div>
+                    <h2> ADD A NEW SUBJECT </h2>
+                    <div>
+                        <input 
+                            type="text"
+                            placeholder='Subject'
+                            value={subject}
+                            onChange={(e) => setSubject(e.target.value)}
+                        />
+                        <button onClick={handleAddNew}> Add </button>
+                        <button onClick={handleHideOverlay}> Cancel </button>
+                    </div>
+                </div>
+            )}
+
+        </div>
     </>
   )
 }
